e995e5fd52ee242bc84986b387f9a37b4857f018
edin93 <il_matador@hotmail.fr>
2020-10-19 21:26:34 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Contains a function.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    Returns the sum of the argument (list) items.
+    '''
+    return sum(input_list)
