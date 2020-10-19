2c4184d79ce5ea9b54f13ff9dd2144368ae42da0
edin93 <il_matador@hotmail.fr>
2020-10-19 21:33:22 +0100

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Contains a function.
+'''
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    '''
+    Return the sum of the argument items as a float.
+    '''
+    return sum(mxd_lst)
