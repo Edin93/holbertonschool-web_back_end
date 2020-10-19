b8cd70eefe8f24bb523ecb9ec8e43c34367b46a6
edin93 <il_matador@hotmail.fr>
2020-10-19 21:01:51 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Contains a concat function.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Returns the 2 arguments concatenated.
+    '''
+    return str1 + str2
