3db96ab095dd0c55b8ddc5e8e74d5c7b764f6837
edin93 <il_matador@hotmail.fr>
2020-10-19 22:42:24 +0100

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Contains a function.
+'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    Return a function that multiplies a float by the argument.
+    '''
+    def multi_func(x: float) -> float:
+        '''
+        Return float.
+        '''
+        return x * multiplier    
+    return multi_func
