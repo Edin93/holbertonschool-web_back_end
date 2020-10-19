377ee9ddef131deea0d3a49d27861be8582c36ea
edin93 <il_matador@hotmail.fr>
2020-10-19 21:10:02 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Contains a floor function.
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''
+    Returns the arguments floated as an integer.
+    '''
+    return math.floor(n)
