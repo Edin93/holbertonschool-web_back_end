248082545ccd400f14a7fd7c023dd19f4efd4e69
edin93 <il_matador@hotmail.fr>
2020-10-21 18:45:06 +0100

    task 0;

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,13 @@ Contains a coroutine.
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    '''
+async def async_generator() -> Generator[float, None, None]:
+    """
     Loops 10 times and yield a random number between 0 and 10.
-    '''
+    """
     for i in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
