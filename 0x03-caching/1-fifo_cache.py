e6828091f62aa113d0440af2b2627daa69533255
edin93 <il_matador@hotmail.fr>
2020-10-26 23:54:42 +0100

    task 1

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+FIFOCache module.
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+        FIFOCache class.
+    """
+
+    def __init__(self):
+        """
+            Initialize.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+            Add an item in the cache.
+        """
+        if not (key is None or item is None):
+            if (
+                len(self.cache_data.keys()) == 4 and
+                (key not in self.cache_data.keys())
+            ):
+                first = sorted(self.cache_data.keys())[0]
+                print('DISCARD: {}'.format(first))
+                del self.cache_data[first]
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            Get an item by key.
+        """
+        if key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
