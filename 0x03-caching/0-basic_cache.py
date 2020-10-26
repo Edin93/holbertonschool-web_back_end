fe9af52ad1dd07df45d92fcd383d16b64ef2aee7
edin93 <il_matador@hotmail.fr>
2020-10-26 23:12:56 +0100

    task 0

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -3,34 +3,31 @@
 """
 
 
-BaseCache = __import__('base_cache').BaseCache
+BaseCaching = __import__('basic_cache').BaseCaching
 
 
-class BasicCache(BaseCache):
+class BasicCache(BaseCaching):
     """ BasicCache defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
+      - inherits from the BaseCaching class.
+      - has a put method to add data to the cache system.
+      - has a get method that returns the value of a given key from the cache.
     """
-    MAX_ITEMS = 4
 
     def __init__(self):
-        """ Initiliaze
+        """ Initialize.
         """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
+        super().__init__()
 
     def put(self, key, item):
         """ Add an item in the cache
         """
-        raise NotImplementedError("put must be implemented in your cache class")
+        if not (key is None and item is None):
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
         """
-        raise NotImplementedError("get must be implemented in your cache class")
+        if (key is None or key not in self.cache_data.keys()):
+            return None
+        else:
+            return self.cache_data[key]
