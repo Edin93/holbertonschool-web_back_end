df3a7200d9ff4a3efc2c88bfd70463a9f3d4bc0b
edin93 <il_matador@hotmail.fr>
2020-10-26 22:52:44 +0100

    caching project init.

--- 0x00-python_variable_annotations/8-main.py
+++ 0x00-python_variable_annotations/8-main.py
@@ -2,5 +2,5 @@
 
 make_multiplier = __import__('8-make_multiplier').make_multiplier
 print(make_multiplier.__annotations__)
-fun = make_multiplier(2.22)
-print("{}".format(fun(2.22)))
+fun = make_multiplier(3)
+print("{}".format(fun(5)))
