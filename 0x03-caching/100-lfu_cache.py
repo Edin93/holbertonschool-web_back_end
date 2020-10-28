9d4f8f55bb5a825779fda2befa7580e48f450b2a
edin93 <il_matador@hotmail.fr>
2020-10-28 23:51:33 +0100

    pagination init

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -2,6 +2,8 @@
 """
 LFUCache module.
 """
+import time
+
 
 BaseCaching = __import__('base_caching').BaseCaching
 
@@ -29,23 +31,32 @@ class LFUCache(BaseCaching):
             ):
                 ordered_keys_by_value = {
                     k: v for k, v in sorted(
-                        self.keys_usage_frequency.items(), key=lambda el: el[1]
-                        )
+                        self.keys_usage_frequency.items(), key=lambda el: el['usage'][1]
+                    )
                 }
                 lfu = list(ordered_keys_by_value.keys())[0]
                 print('DISCARD: {}'.format(lfu))
                 del self.keys_usage_frequency[lfu]
                 del self.cache_data[lfu]
-                self.keys_usage_frequency[key] = 1
+                self.keys_usage_frequency[key] = {
+                    'time': time.time(),
+                    'usage': 1
+                }
                 self.cache_data[key] = item
             elif (
                 len(self.cache_data.keys()) == BaseCaching.MAX_ITEMS and
                 (key in self.cache_data.keys())
             ):
-                self.keys_usage_frequency[key] += 1
+                self.keys_usage_frequency[key] = {
+                    'time': time.time(),
+                    'usage': self.keys_usage_frequency[key]['usage'] + 1
+                }
                 self.cache_data[key] = item
             else:
-                self.keys_usage_frequency[key] = 1
+                self.keys_usage_frequency[key] = {
+                    'time': time.time(),
+                    'usage': 1
+                }
                 self.cache_data[key] = item
 
     def get(self, key):
@@ -55,5 +66,8 @@ class LFUCache(BaseCaching):
         if key not in self.cache_data.keys():
             return None
         else:
-            self.keys_usage_frequency[key] += 1
+            self.keys_usage_frequency[key] = {
+                'time': time.time(),
+                'usage': self.keys_usage_frequency[key]['usage'] + 1
+            }
             return self.cache_data[key]
