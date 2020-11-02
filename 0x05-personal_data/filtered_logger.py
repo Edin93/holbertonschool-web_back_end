569bbeef54f5ec8089ddf3b1c40c2b870c4f6ab6
edin93 <il_matador@hotmail.fr>
2020-11-02 19:39:25 +0100

    update function

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,7 +6,10 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     '''Returns a log message.'''
-    return (';'.join(x if x.split('=')[0] not in fields
-            else re.sub(r'=.*', '=' + redaction, x) for x in message.split(separator)))
+    return (';'.join(x if x.split('=')[0] not in fields else re.sub(
+        r'=.*', '=' + redaction, x) for x in message.split(separator)))
