d4e7e1b03c791969de35c85f6619badea29b5af1
edin93 <il_matador@hotmail.fr>
2020-11-02 18:36:30 +0100

    task 0

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Logging module.
+"""
+
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str, msg: str, sep: str) -> str:
+    '''Returns a log msg.'''
+    return (sep.join(x if x.split('=')[0] not in fields
+            else re.sub(r'=.*', '=' + redaction, x) for x in msg.split(sep)))
