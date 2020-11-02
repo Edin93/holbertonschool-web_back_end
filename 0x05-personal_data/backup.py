b7c5e346514b1327e5c09957b0c962335082ae84
edin93 <il_matador@hotmail.fr>
2020-11-02 21:27:15 +0100

    task 1

---
+++ 0x05-personal_data/backup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Logging module.
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    '''Returns a log message.'''
+    return (separator.join(x if x.split('=')[0] not in fields else re.sub(
+        r'=.*', '=' + redaction, x) for x in message.split(separator)))
--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,8 +2,28 @@
 """
 Logging module.
 """
+import logging
 import re
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Returns a log'''
+        logging.basicConfig(level=record.levelname, format=self.FORMAT)
+        return logging.info(filter_datum(self.fields, self.REDACTION,
+                            record.msg, self.SEPARATOR))
 
 
 def filter_datum(fields: List[str],
