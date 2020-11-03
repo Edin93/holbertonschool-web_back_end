8bb9e002032c671661c006ff703f900a7b80a976
edin93 <il_matador@hotmail.fr>
2020-11-03 09:44:16 +0100

    fix arguments order

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,12 +7,15 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password."""
-    # pw = bytes(password, 'utf-8')
-    pw = password.encode('utf-8')
+    # pw = bytes(password, "utf-8")
+    pw = password.encode("utf-8")
     return bcrypt.hashpw(pw, bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks if the provided password matches the hashed password."""
-    # pw = bytes(password, 'utf-8')
-    return bcrypt.checkpw(hashed_password, password.encode('utf-8'))
+    # pw = bytes(password, "utf-8")
+    if bcrypt.checkpw(password.encode("utf-8"), hashed_password):
+        return True
+    else:
+        return False
