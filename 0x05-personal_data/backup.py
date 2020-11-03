40ec3e2123c18866a027db2ca6481896dfd6a7db
edin93 <il_matador@hotmail.fr>
2020-11-03 01:33:07 +0100

    update task2

--- 0x05-personal_data/backup.py
+++ 0x05-personal_data/backup.py
@@ -2,8 +2,28 @@
 """
 Logging module.
 """
+import logging
 import re
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Returns a log'''
+        msg = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            msg, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str],
--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -42,9 +42,14 @@ def get_logger() -> logging.Logger:
     """Returns a Logger object."""
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
+    logger.propagate = False
+
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
+    
     formatter = logging.Formatter(RedactingFormatter(PII_FIELDS))
     stream_handler.setFormatter(formatter)
+    
     logger.addHandler(stream_handler)
+    
     return logger
