c1f3088dcdacfe184f9f1e97f15513a059ce0592
edin93 <il_matador@hotmail.fr>
2020-11-07 00:26:19 +0100

    task 12

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,7 +61,7 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        credentials = decoded_base64_authorization_header.split(':')
+        credentials = decoded_base64_authorization_header.split(':', 1)
         return (credentials[0], credentials[1])
 
     def user_object_from_credentials(
