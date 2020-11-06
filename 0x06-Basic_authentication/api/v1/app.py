741d927ffe04b1168e8442341b9de6b6fd5d4df6
edin93 <il_matador@hotmail.fr>
2020-11-06 21:50:01 +0100

    task 5 fixed

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -43,7 +43,7 @@ def not_found(error) -> str:
 
 
 @app.before_request
-def before_request():
+def before_request_func():
     """
     Executed before each request that is handled by a function of the
     Blueprint.
@@ -52,9 +52,9 @@ def before_request():
         '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
     ]
     if auth is None:
-        pass
-    if auth.require_auth(request.path, excluded_paths):
-        pass
+        return
+    if not auth.require_auth(request.path, excluded_paths):
+        return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
