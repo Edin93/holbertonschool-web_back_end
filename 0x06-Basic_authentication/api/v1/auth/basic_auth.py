55852dbb31016d91200f7485c009e430c64819ab
edin93 <il_matador@hotmail.fr>
2020-11-06 22:08:00 +0100

    task 6

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,3 +14,17 @@ class BasicAuth(Auth):
         """ Constructor.
         """
         pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header for a Basic
+        Authentication.
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
