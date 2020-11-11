a70b2c146380d2e7c04eff9825cd5692698f73ef
edin93 <il_matador@hotmail.fr>
2020-11-11 22:10:43 +0100

    task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -60,9 +60,8 @@ def before_request_func():
         return
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.session_cookie(request) is None:
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
