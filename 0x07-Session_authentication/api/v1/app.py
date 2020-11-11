91dc8404e1f403750123b13b47dbe2792d725d15
edin93 <il_matador@hotmail.fr>
2020-11-11 22:02:39 +0100

    task 6

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -53,13 +53,16 @@ def before_request_func():
     Blueprint.
     """
     excluded_paths = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth is None:
         return
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
