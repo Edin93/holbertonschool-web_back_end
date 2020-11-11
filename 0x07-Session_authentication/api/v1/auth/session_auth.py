9f7b3eeae542ecf3aa719b7acbbb6041e0993093
edin93 <il_matador@hotmail.fr>
2020-11-11 21:31:43 +0100

    task 2

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Contains SessionAuth class.
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -10,8 +11,22 @@ class SessionAuth(Auth):
     SessionAuth class.
     """
 
+    user_id_by_session_id = {}
+
     def __init__(self):
         """
         Constructor.
         """
         pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session ID for user_id.
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        sessionId = str(uuid.uuid4())
+        self.user_id_by_session_id['user_id'] = sessionId
+        return sessionId
