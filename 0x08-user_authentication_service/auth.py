96e124004bf49a91337be11e190dca61292ee76e
Edin93 <il_matador@hotmail.fr>
2020-11-18 23:08:28 +0100

    task 12

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -72,3 +72,15 @@ class Auth:
                 return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """
+        Returns the user the corresponds to the given session_id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
