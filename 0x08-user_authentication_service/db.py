cc7b5f8f4475338724c2b040267374704114d955
Edin93 <il_matador@hotmail.fr>
2020-11-18 15:53:08 +0100

    task 1 done

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -14,7 +14,7 @@ class DB:
 
     def __init__(self):
         """ DB class initialization """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
