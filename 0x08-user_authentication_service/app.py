ded30609cb21b7ea46b23cdd6f099e3b4155c071
Edin93 <il_matador@hotmail.fr>
2020-11-18 23:27:04 +0100

    task 15

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -64,11 +64,10 @@ def profile() -> str:
     """
     sessiond_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        abort(403)
-    else:
+    if user:
         return jsonify({"email": user.email}), 200
-
+    else:
+        abort(403)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
