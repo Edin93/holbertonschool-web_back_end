9f34f8cbf15d69d9846b6759c54b8e655654555e
Edin93 <il_matador@hotmail.fr>
2020-11-18 15:54:57 +0100

    task 1

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -27,7 +27,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Saves the user to the database and Returns the User object.
         """
