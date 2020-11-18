cf0d89e82f7501a23f05dd3a3df568bb9a5fe859
Edin93 <il_matador@hotmail.fr>
2020-11-18 21:52:20 +0100

    task 6

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_home():
+    """ Returns home page result. """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
