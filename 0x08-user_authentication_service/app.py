a98c73c18183a19da9fd1188300e71e6e892d413
Edin93 <il_matador@hotmail.fr>
2020-11-19 02:18:13 +0100

    task 17 update

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -72,7 +72,7 @@ def profile() -> str:
 
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
-def get_reset_password_token():
+def get_reset_password_token() -> str:
     """
     Reset password token route.
     """
