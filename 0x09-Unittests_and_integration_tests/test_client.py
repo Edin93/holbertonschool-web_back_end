8a120671c8f69b6da785092f3dd85a1f747c4f66
Edin93 <il_matador@hotmail.fr>
2020-12-01 01:19:25 +0100

    task 9

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -104,7 +104,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(ghoc.org, self.org_payload)
         self.assertEqual(ghoc.repos_payload, self.repos_payload)
 
-    def test_public_repos_with_license():
+    def test_public_repos_with_license(self):
         """ Testing public_repos with the argument license="apache-2.0" """
         ghoc = client.GithubOrgClient('random')
         self.assertEqual(ghoc.public_repos('apache-2.0'), self.apache2_repos)
