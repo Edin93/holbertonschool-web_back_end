1a8afa14e74544b5d97070200fe20d8e1bbbc0e7
Edin93 <il_matador@hotmail.fr>
2020-12-01 17:07:25 +0100

    task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -8,15 +8,21 @@ import uuid
 
 
 class Cache:
-    """ Redis caching class. """
+    """
+        Redis caching class.
+    """
 
     def __init__(self):
-        """ Cache class initialization. """
+        """
+            Cache class initialization.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Stores the input data in Redis under a randomly generated key. """
+        """
+            Stores the input data in Redis under a randomly generated key
+        """
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
