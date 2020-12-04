bbdd07aceb9477a1215e08eee4c04407c7a2a425
Edin93 <il_matador@hotmail.fr>
2020-12-04 16:00:22 +0100

    task 0

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create a table 'users'.
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255)
 )
