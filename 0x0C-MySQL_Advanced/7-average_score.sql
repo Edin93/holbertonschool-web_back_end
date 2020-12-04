7dd9d59e06719a5ba6cd44db072122c55b66d053
Edin93 <il_matador@hotmail.fr>
2020-12-04 23:58:59 +0100

    task 7

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
 	UPDATE users
 	SET
-		average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+		average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
 	WHERE
 		id = user_id;
 END//
