e22032aceeae62d5a87d6dc246814c1f72f43a37
Edin93 <il_matador@hotmail.fr>
2020-12-04 23:23:02 +0100

    task 6

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,14 +1,18 @@
 -- Create a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER //
-CREATE PROCEDURE AddBonus(user_id, project_name, score)
+CREATE PROCEDURE AddBonus(
+	user_id INT,
+	project_name VARCHAR(255),
+	score FLOAT
+)
 BEGIN
-                INSERT INTO projects (name)
-                SELECT * FROM (SELECT project_name) AS tmp
-                WHERE NOT EXISTS (
-                        SELECT name FROM projects WHERE name = project_name
-                ) LIMIT 1;
-                @project_id = SELECT id FROM projects WHERE name = project_name;
-                INSERT INTO corrections (user_id, project_id, score)
-                VALUES (user_id, @project_id, score)
-END //
+		INSERT INTO projects (name)
+		SELECT * FROM (SELECT project_name) AS tmp
+		WHERE NOT EXISTS (
+			SELECT name FROM projects WHERE name = project_name
+		) LIMIT 1;
+		SET @project_id = (SELECT id FROM projects WHERE name = project_name);
+		INSERT INTO corrections (user_id, project_id, score)
+		VALUES (user_id, @project_id, score);
+END//
 DELIMITER ;
