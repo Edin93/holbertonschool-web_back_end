0410afb6428dcfc39d5c0bb1030c9dad4e8111d4
Edin93 <il_matador@hotmail.fr>
2020-12-05 05:19:25 +0100

    task 13

--- 0x0C-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,19 +1,19 @@
 -- Creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students.
+
 DELIMITER //
+
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers//
+
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE n INT DEFAULT 0;
-    DECLARE i INT DEFAULT 0;
-    SELECT COUNT(*) FROM users INTO n;
-    SET i = 0;
-    WHILE (i < n) DO
-        UPDATE users SET average_score = (
-            SELECT sum((SELECT weight FROM projects WHERE corrections.project_id = id) * score) / (SELECT sum(weight) FROM projects)
-            FROM corrections
-            WHERE 1 = 1
-        );
-        SET i = i + 1;
-    END WHILE;
+    UPDATE users
+    SET average_score = (
+        SELECT SUM(proj.weight * corr.score) / SUM(proj.weight)
+        FROM corrections AS corr
+        RIGHT JOIN projects AS proj
+        ON corr.project_id = proj.id
+        WHERE corr.user_id = users.id
+    );
 END//
+
 DELIMITER ;
