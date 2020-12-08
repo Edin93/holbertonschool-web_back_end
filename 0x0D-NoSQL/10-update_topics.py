cd32d4bbe294a4b69bf9839f0ff19daf64e1721d
Edin93 <il_matador@hotmail.fr>
2020-12-08 20:55:04 +0100

    task 10

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module contains a function that changes all topics of a school document based on the name.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school documents based on the name.
+    """
+    if mongo_collection and name and topics:
+        mongo_collection.update_one(
+            {'name': name},
+            {
+                '$set': {'topics': topics}
+            }
+        )
