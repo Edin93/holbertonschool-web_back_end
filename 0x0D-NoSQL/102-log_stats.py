bf79c00ea780a646dbcb96b6dbb4bcfab66b293f
Edin93 <il_matador@hotmail.fr>
2020-12-08 22:27:17 +0100

    task 13

---
+++ 0x0D-NoSQL/102-log_stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Python script that provides some stats about Nginx logs stored in MongoDB.
+"""
+from pymongo import MongoClient
+
+
+def log_infos() -> None:
+    """ Provides some stats about Nginx logs stored in MongoDB. """
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+
+    print(
+        "{} logs".format(
+            nginx.count_documents({})
+        )
+    )
+
+    print("Methods:")
+
+    for method in methods:
+        print(
+            "\tmethod {}: {}".format(
+                method, nginx.count_documents({'method': method})
+            )
+        )
+
+    print(
+        "{} status check".format(
+            nginx.count_documents({'method': 'GET', 'path': '/status'})
+        )
+    )
+
+
+if __name__ == "__main__":
+    log_infos()
