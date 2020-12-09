e46ada915decae7d71b593c01a0da98b62f54b86
Edin93 <il_matador@hotmail.fr>
2020-12-09 01:41:40 +0100

    task 14

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -8,11 +8,31 @@ Module contains a function that returns all students sorted by average score.
 def top_students(mongo_collection):
     """ Returns all the students sorted by average score. """
 
-    result = mongo_collection.aggregrate([
-        {
-            "$project": {
-                "averageScore": { "$size": "$topics"}
-            }
+    students = []
+
+    for document in mongo_collection.find():
+
+        topics = document.get('topics')
+        sum_score = 0
+
+        for topic in topics:
+            sum_score += topic.get('score', 0)
+
+        averageScore = sum_score / len(topics)
+
+        student = {
+            'name': document.get('name'),
+            '_id': document.get('_id'),
+            'averageScore': averageScore
         }
-    ])
-    return result
+
+        limit = len(students)
+        if limit == 0:
+            students.append(student)
+        else:
+            i = 0
+            while i < limit and students[i]['averageScore'] > student['averageScore']:
+                i += 1
+            students.insert(i, student)
+
+    return students
