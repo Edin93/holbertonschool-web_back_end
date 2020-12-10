870d7c8868ad20f88f1b3e9e5076ab23ce304607
Edin93 <il_matador@hotmail.fr>
2020-12-10 21:13:57 +0100

    task 11

--- 0x0E-ES6_basic/11-createEmployeesObject.js
+++ 0x0E-ES6_basic/11-createEmployeesObject.js
@@ -1,10 +1,10 @@
-export default function createEmployeesObject (departmentName, employees) {
+export default function createEmployeesObject(departmentName, employees) {
   const val = [];
   for (const i of employees) {
     val.push(i);
   }
   const obj = {
-    [`${departmentName}`]: val
+    [`${departmentName}`]: val,
   };
 
   return obj;
