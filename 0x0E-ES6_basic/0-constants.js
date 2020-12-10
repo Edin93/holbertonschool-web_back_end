48beaa750f96f80f4d2422943e597bf93d8bfe2b
Edin93 <il_matador@hotmail.fr>
2020-12-10 15:28:21 +0100

    task 0

--- 0x0E-ES6_basic/0-constants.js
+++ 0x0E-ES6_basic/0-constants.js
@@ -8,7 +8,7 @@ export function getLast () {
 }
 
 export function taskNext () {
-  let combination = `But sometimes let ${getLast()}`;
-
+  let combination = 'But sometimes let';
+  combination += getLast();
   return combination;
 }
