025bd09f761f65f471c87517aa7cc6663beb5852
Edin93 <il_matador@hotmail.fr>
2020-12-16 22:54:23 +0100

    task 10

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -1,20 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return Promise.all([
-    uploadPhoto(),
-    createUser(),
-  ]).then((data) => ({
-    photo: {
-      status: data[0].status,
-      body: data[0].body,
-    },
-    user: {
-      firstName: data[1].firstName,
-      lastname: data[1].lastName,
-    },
-  })).catch(() => ({
-    photo: null,
-    user: null,
-  }));
+	try {
+		const user = createUser();
+		const photo = uploadPhoto();
+
+		return {
+			photo: photo.then((data) => data),
+			user: user.then((data) => data)
+		};
+	}
+	catch (e) {
+		return {
+			photo: null,
+			user: null
+		}
+	}
 }
