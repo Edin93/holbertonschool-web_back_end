f25b527e6bda2b7fa02b5c6eb139953fc1f6ebed
Edin93 <il_matador@hotmail.fr>
2020-12-16 20:59:24 +0100

    task 3

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -2,11 +2,13 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   let msg = '';
-  Promise.all([
+  return Promise.all([
     uploadPhoto(),
     createUser(),
   ]).then((data) => {
     msg += `${data[0].body} ${data[1].firstName} ${data[1].lastName}`;
     console.log(msg);
+  }).catch(() => {
+    console.log('Signup system offline');
   });
 }
