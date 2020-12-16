57c21cc39eaf87e778738c6d152c7c56c345afd2
Edin93 <il_matador@hotmail.fr>
2020-12-16 20:35:43 +0100

    task 2

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
+  console.log('Got a response from the API');
   promise.then(() => ({
     status: 200,
     body: 'success',
   })).catch(() => new Error());
-  console.log('Got a response from the API');
 }
