b800772f40cf3544f473692ebc47ab296e0e954a
Edin93 <il_matador@hotmail.fr>
2020-12-16 22:28:15 +0100

    task 8

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,8 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-	if (denominator !== 0) {
-		return (numerator / denominator);
-	}
-	else {
-		throw new Error('cannot divide by 0');
-	}
+  if (denominator !== 0) {
+    return (numerator / denominator);
+  }
+
+  throw new Error('cannot divide by 0');
 }
