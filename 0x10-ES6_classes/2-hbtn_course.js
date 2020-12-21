de3e9283cb01a340436715b3522cd5481a963026
Edin93 <il_matador@hotmail.fr>
2020-12-21 23:08:07 +0100

    update tasks

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (students) {
+      this._students = students;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+}
