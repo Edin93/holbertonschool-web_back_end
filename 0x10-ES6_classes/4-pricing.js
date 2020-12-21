aea74c0ede0148da3fb443a12a1179dcf3af6878
Edin93 <il_matador@hotmail.fr>
2020-12-21 23:37:49 +0100

    update tasks

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw TypeError('currency must be of type Currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
