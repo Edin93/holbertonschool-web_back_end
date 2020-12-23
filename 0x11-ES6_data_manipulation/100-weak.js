a35ecbe5849374e2698d5fdbe0844ff52772122b
Edin93 <il_matador@hotmail.fr>
2020-12-23 22:12:18 +0100

    tasks update

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -3,9 +3,10 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const n = weakMap.get(endpoint);
-    weakMap.set(endpoint, n + 1);
     if (n >= 5) {
-      throw Error('Endpoint load is high');
+      throw new Error('Endpoint load is high');
+    } else {
+      weakMap.set(endpoint, n + 1);
     }
   } else {
     weakMap.set(endpoint, 1);
