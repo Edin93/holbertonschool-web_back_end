b594c0dd2e1ecbb81d95d27fcfdc44ba92cd88b8
Edin93 <il_matador@hotmail.fr>
2020-12-23 18:31:46 +0100

    tasks update

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-	try {
-		let buffer = new Int8Array(length, position);
-		buffer[0] = value;
-		return buffer;
-
-	} catch (error) {
-		throw Error('Position outside range');
-	}
+  try {
+    const buffer = new ArrayBuffer(length);
+    const data = new DataView(buffer);
+    data.setInt8(position, value);
+    return data;
+  } catch (error) {
+    throw Error('Position outside range');
+  }
 }
