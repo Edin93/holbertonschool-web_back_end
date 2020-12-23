4afedfa4683dd6942cbdf0bc1998af5ad6a3a08f
Edin93 <il_matador@hotmail.fr>
2020-12-23 22:28:14 +0100

    tasks update

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,14 @@ export default function cleanSet(set, startString) {
   if (typeof startString !== 'string') return '';
   if (startString.length === 0) return '';
   if (typeof set !== 'object') return '';
+
   const str = [];
+
   set.forEach((el) => {
-    if (el.startsWith(startString)) {
+    if (el && el.startsWith(startString)) {
       str.push(el.substr(startString.length));
     }
   });
+
   return str.join('-');
 }
