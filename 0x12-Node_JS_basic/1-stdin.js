e5ff70dda59ee2f7e167d153838a769dc9450b94
Edin93 <il_matador@hotmail.fr>
2021-01-14 19:36:28 +0100

    task 1

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
