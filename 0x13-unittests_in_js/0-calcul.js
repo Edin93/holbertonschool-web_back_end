57ec0527f4dbe007e6111e24212708a6bd60528e
Edin93 <il_matador@hotmail.fr>
2021-01-25 17:27:35 +0100

    task 0

--- 0x13-unittests_in_js/0-calcul.js
+++ 0x13-unittests_in_js/0-calcul.js
@@ -2,5 +2,4 @@ function calculateNumber(a, b) {
 	return (Math.round(a) + Math.round(b));
 }
 
-module.exports = { calculateNumber };
-
+module.exports = calculateNumber;
--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,9 @@
-const { calculateNumber } = require("./0-calcul");
+const calculateNumber = require("./0-calcul");
+const assert = require('assert');
 
-// let assert = require('assert');
-
-// describe("calculateNumber", function() {
-// 	assert.equal(4, calculateNumber(1, 3));
-// 	assert.equal(5, calculateNumber(1, 3.7));
-// 	assert.equal(5, calculateNumber(1.2, 3.7));
-// 	assert.equal(6, calculateNumber(1.5, 3.7));
-// });
-
-console.log("results");
-
-console.log(calculateNumber(1, 3));
-console.log(calculateNumber(1, 3.7));
-console.log(calculateNumber(1.2, 3.7));
-console.log(calculateNumber(1.5, 3.7));
+describe("calculateNumber", () => {
+	assert.equal(4, calculateNumber(1, 3));
+	assert.equal(5, calculateNumber(1, 3.7));
+	assert.equal(5, calculateNumber(1.2, 3.7));
+	assert.equal(6, calculateNumber(1.5, 3.7));
+});
