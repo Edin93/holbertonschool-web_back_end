7cc98a4ad2de114779decc75d78a739b97f7cc14
Edin93 <il_matador@hotmail.fr>
2021-01-25 17:34:30 +0100

    task 0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,6 +4,8 @@ const assert = require('assert');
 describe("calculateNumber", () => {
 	assert.equal(4, calculateNumber(1, 3))
 	assert.equal(5, calculateNumber(1.8, 3));
+	assert.equal(5, calculateNumber(1.8, 3.2));
+	assert.equal(4, calculateNumber(1, 3.2));
 	assert.equal(5, calculateNumber(1, 3.7));
 	assert.equal(5, calculateNumber(1.2, 3.7));
 	assert.equal(6, calculateNumber(1.5, 3.7));
