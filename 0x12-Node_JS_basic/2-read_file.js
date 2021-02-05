bd8b3976fbfd53e221bee840565c09a8b4b17689
Edin93 <il_matador@hotmail.fr>
2021-02-05 17:25:42 +0100

    update task 2 to synchronous file reading

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,53 +1,44 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path,
-    'utf-8',
-    (error, results) => {
-      if (error) {
-        throw new Error('Cannot load the database');
-      } else {
-        const lines = results.split('\n');
-        let i = 0;
-        let countStudents = 0;
-        const fields = {};
+  try {
+    const results = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
+    const lines = results;
+    let i = 0;
+    let countStudents = 0;
+    const fields = {};
 
-        const getLines = () => {
-          for (const line of lines) {
-            if (line.trim() !== '' && i > 0) {
-              countStudents += 1;
-            	const [fname, lname, age, field] = line.split(','); // eslint-disable-line
-              if (!fields[field]) {
-                fields[field] = {
-                  count: 1,
-                  students: [fname],
-                };
-              } else {
-                const newCount = fields[field].count + 1;
-                const newStudents = (fields[field].students).concat(fname);
-                fields[field] = {
-                  count: newCount,
-                  students: newStudents,
-                };
-              }
-            }
-            i += 1;
-          }
-        };
-
-        const display = async () => {
-          getLines();
-          console.log(`Number of students: ${countStudents}`);
-          for (const field of Object.keys(fields)) {
-            const n = fields[field].count;
-            const names = fields[field].students.join(', ');
-            console.log(`Number of students in ${field}: ${n}. List: ${names}`);
-          }
-        };
-
-        display();
+    for (const line of lines) {
+      if (line.trim() !== '' && i > 0) {
+        countStudents += 1;
+				const [fname, lname, age, field] = line.split(','); // eslint-disable-line
+        if (!fields[field]) {
+          fields[field] = {
+            count: 1,
+            students: [fname],
+          };
+        } else {
+          const newCount = fields[field].count + 1;
+          const newStudents = (fields[field].students).concat(fname);
+          fields[field] = {
+            count: newCount,
+            students: newStudents,
+          };
+        }
       }
-    });
+      i += 1;
+    }
+
+    console.log(`Number of students: ${countStudents}`);
+    for (const field of Object.keys(fields)) {
+      const n = fields[field].count;
+      const names = fields[field].students.join(', ');
+      // console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
