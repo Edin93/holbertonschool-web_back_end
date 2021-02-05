698dae1ecc742de1de8a9e4a1dadd77260ba2329
Edin93 <il_matador@hotmail.fr>
2021-02-05 16:19:31 +0100

    nodejs project, task 2

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  fs.readFile(path, 'utf-8', (error, results) => {
+    if (error) {
+      throw new Error('Cannot load the database');
+    } else {
+      const lines = results.split('\n');
+      let i = 0;
+      const fields = {};
+
+      const getLines = () => {
+        for (const line of lines) {
+          if (line.trim() !== '' && i > 0) {
+            const [fname, lname, age, field] = line.split(','); // eslint-disable-line
+            if (!fields[field]) {
+              fields[field] = {
+                count: 1,
+                students: [fname],
+              };
+            } else {
+              const newCount = fields[field].count + 1;
+              const newStudents = (fields[field].students).concat(fname);
+              fields[field] = {
+                count: newCount,
+                students: newStudents,
+              };
+            }
+          }
+          i += 1;
+        }
+      };
+
+      const display = async () => {
+        getLines();
+        console.log(`Number of students: ${i}`);
+        for (const field of Object.keys(fields)) {
+          const n = fields[field].count;
+          const names = fields[field].students.join(', ');
+          console.log(`Number of students in ${n}: 6. List: ${names}`);
+        }
+      };
+
+      display();
+    }
+  });
+}
+
+module.exports = countStudents;
